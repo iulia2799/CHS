09e2f0a74a2a45d77c82cff1339bbfa0cc05fdef
iulia2799 <iuliasarah27@gmail.com>
2021-11-25 17:40:57 +0200

    connected to firebase.

--- app/src/main/java/com/example/chs/AddPost.java
+++ app/src/main/java/com/example/chs/AddPost.java
@@ -2,9 +2,12 @@ package com.example.chs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddPost extends AppCompatActivity {
     private TextView addpost;
@@ -21,5 +24,12 @@ public class AddPost extends AppCompatActivity {
         desc = (TextView) findViewById(R.id.desc);
         searchlocation = (TextView) findViewById(R.id.location_gps);
         anonymous = (Switch) findViewById(R.id.anon);
+    }
+    public void clickAddPost(View view){
+        Context context = getApplicationContext();
+        String text = "the post was added";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
     }
 }
