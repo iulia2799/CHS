e563f95bb12a5b5d1c4f67df27d4aa0a101bc01a
iulia2799 <iuliasarah27@gmail.com>
2022-06-01 19:11:38 +0300

    email verification

--- app/src/main/java/com/example/chs/SignUp.java
+++ app/src/main/java/com/example/chs/SignUp.java
@@ -1,7 +1,11 @@
 package com.example.chs;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,13 +19,24 @@ import android.widget.Toast;
 import com.example.chs.data.login.DAOPrimarie;
 import com.example.chs.data.login.DAOUser;
 import com.example.chs.data.login.Primarie;
+import com.example.chs.data.login.PrimarieLocalStorage;
 import com.example.chs.data.login.User;
+import com.example.chs.data.login.UserLocalStorage;
 import com.example.chs.data.model.Alert;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -36,6 +51,7 @@ public class SignUp extends AppCompatActivity {
     private TextView q;
     private EditText confirmaParola;
     private TextView confirmatext;
+    private AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,7 +95,50 @@ public class SignUp extends AppCompatActivity {
         }
         return true;
     }
-    public void clickSignUp(View view){
+    public void clickSignUp(View view) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnSuccessListener(authResult -> {
+            FirebaseUser user = authResult.getUser();
+            assert user != null;
+            user.sendEmailVerification().addOnCompleteListener(task -> {
+                Toast.makeText(this,"Confirma email-ul", Toast.LENGTH_LONG).show();
+            });
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("A fost trimis un mesaj catre email-ul dvs. Va rog sa urmati instructiunile.");
+            builder1.setCancelable(true);
+
+            builder1.setPositiveButton(
+                    "Am confirmat",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Objects.requireNonNull(firebaseAuth.getCurrentUser()).reload();
+                            FirebaseAuth.getInstance().getCurrentUser().reload().addOnSuccessListener(unused -> {
+                                FirebaseUser x = firebaseAuth.getCurrentUser();
+                                System.out.println(x.getEmail()+x.isEmailVerified());
+                                if(x.isEmailVerified()) click();
+                            });
+
+
+                        }
+                    });
+
+            builder1.setNegativeButton(
+                    "Nu am primit",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+
+
+        }).addOnFailureListener(e -> {
+          Toast.makeText(this,"Email-ul introdus nu exista!", Toast.LENGTH_SHORT).show();
+        });
+    }
+    public void click(){
         DAOUser daoUser = new DAOUser();
         List<Alert> alertList = new ArrayList<>();
         alertList.add(new Alert("0","Welcome to app"));
@@ -98,6 +157,9 @@ public class SignUp extends AppCompatActivity {
             Primarie primarie = new Primarie(email.getText().toString(),username.getText().toString(),pass.getText().toString());
             daoPrimarie.add(primarie).addOnSuccessListener(suc -> {
                 Toast.makeText(getApplicationContext(), "Adaugat cu succes", Toast.LENGTH_SHORT).show();
+                PrimarieLocalStorage userLocalStorage = new PrimarieLocalStorage(this);
+                userLocalStorage.storeUserData(primarie);
+                userLocalStorage.setUserLoggedIn(true);
                 Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
                 startActivity(intent);
 
@@ -109,6 +171,9 @@ public class SignUp extends AppCompatActivity {
         }
         daoUser.add(user).addOnSuccessListener(suc -> {
             Toast.makeText(getApplicationContext(), "Adaugat cu succes", Toast.LENGTH_SHORT).show();
+            UserLocalStorage userLocalStorage = new UserLocalStorage(this);
+            userLocalStorage.storeUserData(user);
+            userLocalStorage.setUserLoggedIn(true);
             Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
             startActivity(intent);
 
